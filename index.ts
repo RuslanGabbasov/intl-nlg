adef5395f18fb42419f4f4222a6948d026270f00
Ruslan Gabbasov <ruslan.gabbasov@gmail.com>
2022-01-24 00:09:28 +0500

    add plural

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import IntlMessageFormat from "intl-messageformat";
 import RussianNouns from 'russian-nouns-js';
 import './arrays';
 
+const rne = new RussianNouns.Engine();
+
 declare global {
     var NLG_DEBUG: boolean;
 }
@@ -10,6 +12,10 @@ global.NLG_DEBUG = false;
 
 interface RandOptions {
   sep: string
+}
+
+interface PluralOptions {
+  gender: typeof RussianNouns.Gender.COMMON | typeof RussianNouns.Gender.MASCULINE | typeof RussianNouns.Gender.FEMININE | typeof RussianNouns.Gender.NEUTER;
 }
 
 interface EnumOptions {
@@ -36,12 +42,19 @@ const nlg = {
         ? `${values.slice(0, values.length - 1).join(`${opts.sep} `)} ${opts.last} ${values.reverse()[0]}`
         : values.join(`${opts.sep} `)
       : content;
+  },
+  plural: (content: Array<any>) => {
+    NLG_DEBUG && console.log("enum", content)
+    const opts: PluralOptions = content.find(c => c.opts)?.opts || {gender: RussianNouns.Gender.MASCULINE};
+    const values: Array<string> = content.map(c =>
+      rne.pluralize(RussianNouns.createLemma({text: c.v || c, gender: opts.gender}))
+    ).flat();
+    return values.join(' ');
   }
-
 }
 
 console.log(new IntlMessageFormat(
-  `Привет <rand>разные варианты {f1}</rand>
+  `<plural>Привет</plural> от старых штиблет <rand>разные варианты {f1}</rand>
  <enum>{f2}</enum> или <enum>{f3}</enum>`,
   "ru-RU").format(
       {
