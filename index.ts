fd763e1e458706c5d19dd3ecad47a00e22af4945
Ruslan Gabbasov <ruslan.gabbasov@gmail.com>
2022-01-24 09:07:00 +0500

    add decline

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ declare global {
     var NLG_DEBUG: boolean;
 }
 
-global.NLG_DEBUG = false;
+global.NLG_DEBUG = true;
 
 interface RandOptions {
   sep: string
@@ -16,6 +16,16 @@ interface RandOptions {
 
 interface PluralOptions {
   gender: typeof RussianNouns.Gender.COMMON | typeof RussianNouns.Gender.MASCULINE | typeof RussianNouns.Gender.FEMININE | typeof RussianNouns.Gender.NEUTER;
+}
+
+interface DeclineOptions {
+  case: typeof RussianNouns.Case.NOMINATIVE |
+        typeof RussianNouns.Case.GENITIVE |
+        typeof RussianNouns.Case.DATIVE |
+        typeof RussianNouns.Case.ACCUSATIVE |
+        typeof RussianNouns.Case.LOCATIVE |
+        typeof RussianNouns.Case.INSTRUMENTAL |
+        typeof RussianNouns.Case.PREPOSITIONAL;
 }
 
 interface EnumOptions {
@@ -44,20 +54,29 @@ const nlg = {
       : content;
   },
   plural: (content: Array<any>) => {
-    NLG_DEBUG && console.log("enum", content)
+    NLG_DEBUG && console.log("plural", content)
     const opts: PluralOptions = content.find(c => c.opts)?.opts || {gender: RussianNouns.Gender.MASCULINE};
     const values: Array<string> = content.map(c =>
-      rne.pluralize(RussianNouns.createLemma({text: c.v || c, gender: opts.gender}))
+      c.v && c.v.length
+        ? nlg.plural(c.v)
+        : rne.pluralize(RussianNouns.createLemma({text: c.v || c, gender: opts.gender}))
     ).flat();
     return values.join(' ');
+  },
+  decline: (content: Array<any>) => {
+    NLG_DEBUG && console.log("decline", content)
+    const opts: DeclineOptions = content.find(c => c.opts)?.opts || {case: RussianNouns.Case.NOMINATIVE};
   }
 }
 
 console.log(new IntlMessageFormat(
-  `<plural>Привет</plural> от старых штиблет <rand>разные варианты {f1}</rand>
+  `<plural>Привет</plural> от <decline>{f5}</decline>старых <enum><plural>{f4}</plural></enum> и штиблет <rand>разные варианты {f1}</rand>
  <enum>{f2}</enum> или <enum>{f3}</enum>`,
   "ru-RU").format(
       {
-        f1: {v: ["first", "second", "third"], opts: {sep: ' '}},
+        f1: {v: ["сыр", "мясо", "колбаса"], opts: {sep: ' '}},
         f2: "Спартак ЦСКА",
-        f3: {v: ["Спартак", "ЦСКА", "Барселона"], opts: {sep: ",", last: "и"}}, ...nlg}))
+        f3: {v: ["Спартак", "ЦСКА", "Барселона"], opts: {sep: ",", last: "и"}},
+        f4: {v: ["лошадь", "корова", "птица"], opts: {gender: RussianNouns.Gender.FEMININE}},
+        f5: {v: "Локомотив", opts: {dec: RussianNouns.Case.DATIVE}},
+        ...nlg}))
